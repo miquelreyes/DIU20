72db258a1a6e84c93f3e1441eb1152fbb0d4de61
Miquel Reyes <48360554+miquelreyes@users.noreply.github.com>
2020-03-15 21:32:37 +0100

    Add files via upload

---
+++ P1/UX-DIU-Toolkit/Journey.js
@@ -0,0 +1,153 @@
+﻿/*******************************************/
+/*             JOURNEY.JS                  */
+/*     Datos para USER JOURNEY MAP         */   
+/*          [DIU] UX Toolkit v1.0 2019     */                        
+/*          ver 1.1 26/Feb/2020            */
+/*******************************************/
+    
+/****  README:       */
+/****  v.1.1 Incluye nombre de tu grupo de prácticas (Grupo.ID), curso académico y enlace a github ***/
+/****  Modifica los datos para los Journey Map (uno para cada Persona)  */
+/****  Usa los 6 pasos y sigue las instrucciones */   
+/****  Las imagenes para  'Photo', 'feelX', 'imaX' están en carpeta ./photos **/
+/****  Si se usan nuevas imágenes se deben añadir a esa carpeta **/
+/****  Los valores de rating están entre 1..5 **/
+/****  recursos de imágenes:  https://www.vectorstock.com/royalty-free-vectors/vectors-by_zdeneksasek ***/
+
+
+
+
+angular.module("angular", [])
+	.controller("controller", ["$scope", function($scope) { 
+		$scope.Grupo_ID ="DIU3.MQ";
+        $scope.Curso ="2019/20";
+        $scope.Github_ID ="https://github.com/miquelreyes/DIU20";
+        
+		$scope.JourneyIndex = 0;
+        
+        $scope.Journeys = [
+			{		
+                
+                /*************************************/
+                /**** PRIMER USER JOURNEY MAP  *******/
+                /*************************************/
+                
+				Id: 0,
+				Name: "Andrés Pérez",
+                Photo: "andres_perez.png",
+    
+                /*** PASO #1: INSPIRACION ***/ 
+                goal1: "Quiere asistir a algún concierto.",
+                touch1: "Móvil (páginas web de salas de conciertos).",
+                feel1: "2",
+                con1: "Todos los conciertos en sala son muy caros.",
+                ima1: "cartoon-phone.png",
+				
+                /*** PASO #2: DECISION ***/ 
+                goal2: "Busca en internet como ir a conciertos baratos.",
+                touch2: "Móvil (buscador).",
+                feel2: "3",
+                con2: "Encuentra varias páginas de compartición de ocio, en este caso música.",
+                ima2: "cartoon-looking.png",
+                
+                /*** PASO #3: ACTUA ***/ 
+                
+                goal3: "Decide usar la que más le llama la atención.",
+                touch3: "Móvil (web).",
+                feel3: "4",
+                con3: "No tienen aplicación movil, solo en navegador.",
+                ima3: "cartoon-phone.png",
+                
+                /*** PASO #4: OBSERVA ***/ 
+                
+                goal4: "Busca un concierto que le parezca interesante para esta semana.",
+                touch4: "Móvil (web).",
+                feel4: "2",
+                con4: "No hay demasiados conciertos en su ciudad en las próximas semanas.",
+                ima4: "cartoon-KO.png",
+                
+                 /*** PASO #5: ANALIZA ***/ 
+                
+                goal5: "Ve que puede suscribirse a una newsletter para que le avisen cuando se organicen eventos cerca de su ciudad.",
+                touch5: "Móvil (web).",
+                feel5: "4",
+                con5: "Tendrá que esperar o seguir buscando en otras web.",
+                ima5: "cartoon-phone.png",
+                
+                
+                /*** PASO #6: CONCLUSION ***/ 
+                
+                goal6: "Ahora estará pendiente de los posibles planes que le puedan surgir.",
+                touch6: "Móvil.",
+                feel6: "3",
+                con6: "Aun no tiene planes para esta semana pero seguirá buscando.",
+                ima6: "cartoon-resting.png",
+                
+			},
+			{	
+                /*************************************/
+                /**** SEGUNDO USER JOURNEY MAP *******/
+                /*************************************/
+                
+				Id: 1,
+				Name: "Maribel González",
+                Photo: "maribel_gonzalez.png",
+                
+				 /*** PASO #1: INSPIRACION ***/ 
+                goal1: "Quiere programar algún evento para darle vida a su bar.",
+                touch1: "Periódico.",
+                feel1: "3",
+                con1: "No sabe con quien hablar o qué hacer.",
+                ima1: "cartoon-why.png",
+                
+                /*** PASO #2: DECISION ***/ 
+                goal2: "Le pregunta a un hijo suyo si conoce algún artista freelance.",
+                touch2: "Hijo.",
+                feel2: "4",
+                con2: "Le cuenta la existencia de apps de compartición de ocio.",
+                ima2: "cartoon-speaking.png",
+                
+                /*** PASO #3: ACTUA ***/ 
+                
+                goal3: "Se mete en una de las primeras webs que le salen.",
+                touch3: "Móvil (web).",
+                feel3: "3",
+                con3: "Permite a la gente acordar con artistas para darles un sitio donde tocar.",
+                ima3: "cartoon-looking.png",
+                
+                /*** PASO #4: OBSERVA ***/ 
+                
+                goal4: "Se crea una cuenta y contacta con uno de los artistas que cobran menos para contratarle un día y probar qué tal.",
+                touch4: "Móvil (web y llamada).",
+                feel4: "2",
+                con4: "Hay mucha oferta de artistas y no está segura de cuál encaja mejor con sus intereses.",
+                ima4: "cartoon-phoning.png",
+                
+                 /*** PASO #5: ANALIZA ***/ 
+                
+                goal5: "Empiezan a acordar un día y las condiciones.",
+                touch5: "Móvil (llamada).",
+                feel5: "3",
+                con5: "El artista le pide más de lo esperado",
+                ima5: "cartoon-shaking.png",
+
+                
+                /*** PASO #6: CONCLUSION ***/ 
+                
+                goal6: "El artista tocará la semana siguiente y empieza a anunciar el evento a sus clientes y cuelga carteles avisando.",
+                touch6: "Clientes.",
+                feel6: "4",
+                con6: "Está contenta porque el artista se encarga de todo el material de sonido y no tiene que organizar nada especial más que guardarle sitio en el bar.",
+                ima6: "cartoon-going.png",
+                
+                
+                
+			}
+		];
+        
+		$scope.model = $scope.Journeys[0];
+
+	}])
+
+
+
---
+++ P1/UX-DIU-Toolkit/Persona.js
@@ -0,0 +1,108 @@
+﻿/*******************************************/
+/*             PERSONA.JS                  */
+/*     Datos para PERSONA TEMPLATE         */   
+/*          [DIU] UX Toolkit v1.0 2019    */                        
+/*          ver 1.1 26/Feb/2020            */
+/*******************************************/
+    
+/****  README:       */
+/****  Modifica los datos para las Personas      */
+/****  v.1.1 Incluye nombre de tu grupo de prácticas (Grupo.ID), curso académico y enlace a github ***/
+/****  Las imagenes para  'Photo'  están en carpeta ./photos **/
+/****  Si se usan nuevas imágenes se deben añadir a esa carpeta **/
+/****  Los valores de rating están entre 1..5 **/
+/****  recursos de imágenes:  https://www.vectorstock.com/royalty-free-vectors/vectors-by_zdeneksasek ***/
+
+
+
+angular.module("angular", [])
+	.controller("controller", ["$scope", function($scope) { 
+        $scope.Grupo_ID ="DIU3.MQ";
+        $scope.Curso ="2019/20";
+        $scope.Github_ID ="https://github.com/miquelreyes/DIU20";
+        
+		$scope.PersonaIndex = 0;
+		$scope.Personas = [
+			{		
+                
+                
+                /*************************************/
+                /**** PRIMERA PERSONA          *******/
+                /*************************************/
+                
+                
+				Id: 0,
+				Name: "Andrés Pérez",
+				Photo: "andres_perez.png",
+				Quote: "Bueno, bonito y barato",
+				Age: 26,
+				Occupation: "Dependiente y estudiante.",
+				Family: "Soltero.",
+				Location: "Granada, España.",
+				Character: "Eficiente y ahorrador",
+				PersonalityTraits: [
+					{ Name: "Introvertido VS  Extrovertido ", Value: 3 },
+					{ Name: "Pasivo  VS    Activo", Value: 1 },
+					{ Name: "Racional  VS   Intuitivo", Value: 3 }
+				], 
+				Goals: ["Música y entretenimiento a buen precio.", "Conocer gente y música nueva."],
+				Frustrations: ["Conciertos demasiado caros.", "Oferta cultural repetitiva y aburrida."],
+				Bio: "Andrés es un joven granadino que está estudiando un módulo y a la vez necesita trabajar de dependiente a media jornada para cubrir sus gastos. Le encanta la música, sobretodo en directo, pero normalmente es incapaz de pagar el coste de un concierto o festival. Además está cansado de ver siempre anunciados conciertos de los mismos artistas. Busca una manera de acceder a música en directo y conocer nuevos cantantes y grupos de la escena pero sin descuidar su bolsillo.",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 3 },
+					{ Name: "Movil", Value: 4 },
+					{ Name: "RRSS", Value: 3 },
+					{ Name: "Software", Value: 2 }
+					
+				], 
+                Contextos: "Lleva tiempo cansado de tener los mismos planes o tener que gastarse mucho dinero para ir a algún concierto",  
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional", Value: 2 },
+					{ Name: "Online & Social Media", Value: 4 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
+				]
+			},
+			{	
+                
+                /*************************************/
+                /**** SEGUNDA PERSONA          *******/
+                /*************************************/
+                
+                
+				Id: 1,
+				Name: "Maribel González",
+				Photo: "maribel_gonzalez.png",
+				Quote: "Reinventarse o morir",
+				Age: 43,
+				Occupation: "Dueña de un bar",
+				Family: "Divorciada con 2 hijos",
+				Location: "Madrid, España.",
+				Character: "Emprendedora.",
+				PersonalityTraits: [
+					{ Name: "Introvertido VS  Extrovertido ", Value: 4 },
+					{ Name: "Pasivo  VS    Activo", Value: 5 },
+					{ Name: "Racional  VS   Intuitivo", Value: 3 }
+				], 
+				Goals: ["Encontrar una nueva fuente de ingresos.", "Renovar el ambiente de su bar.", "Atraer a nuevos tipos de clientes."],
+				Frustrations: ["Las complicaciones de organizar eventos ella sola.", "No estar metida en la escena musical y no tener contactos de artistas."],
+				Bio: "Maribel es la dueña y principal encargada de un bar de Malasaña desde hace más de 15 años. Es una persona inquieta que está siempre buscando nuevas maneras de hacer negocio y de actualizarse a los nuevos tiempos. Siempre intenta renovarse a si misma y a su bar y ahora busca acoger eventos como conciertos en su local sin las complicaciones que implica ser un organizador o entrar en contacto y contratar artistas.",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 2 },
+					{ Name: "Mobile", Value: 3 },
+					{ Name: "RRSS", Value: 2 },
+					{ Name: "Software", Value: 1 }
+					
+				], 
+                Contextos:   "Acoger eventos sin los dolores de cabeza de un organizador." ,
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional (Ads)", Value: 4 },
+					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
+				]
+			}
+		];
+		$scope.model = $scope.Personas[0];
+
+	}])
